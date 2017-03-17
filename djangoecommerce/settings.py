beb6abefc92a64d3716a05fd67b251e984a3c960
alisonamerico <alison.americo@gmail.com>
2017-03-16 23:43:33 -0300

    config adicionais no settings

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-ROOT_URLCONF = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -26,7 +27,7 @@ SECRET_KEY = '45ko^6bkxf3*ip*i6_3lbenn+r0ebkf0se---505#oqq_9ufiq'
 DEBUG = True
 
 # Allow all host headers
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -123,8 +124,11 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 #Update databases configuration with $DATABASES_URL.
-db_from_env = dj_database_url.conf(conn_max_age=500)
-DATABASES['default'].update(db(db_from_env))
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
