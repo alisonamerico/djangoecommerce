b4e89b25aecd087fa05d16341054e4a8208ba4d1
alisonamerico <alison.americo@gmail.com>
2017-04-14 12:58:51 -0300

    adicionando lógica de criar o carrinho de compras

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# coding=utf-8
 
-# Create your views here.
+from django.shortcuts import get_object_or_404
+from django.views.generic import RedirectView
+from django.contrib import messages
+
+from catalog.models import Product
+
+from .models import CartItem
+
+
+class CreateCartItemView(RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        product = get_object_or_404(Product, slug = self.kwargs['slug'])
+        cart_item = CartItem.objects.add_item(
+        self.request.session.session_key, product
+        )
+        messages.success(self.request, 'Produto adicionado com sucesso')
+        return product.get_absolute_url()
+
+
+create_cartitem = CreateCartItemView.as_view()
