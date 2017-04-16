118e695826dc7fc5f37d05d7ce739aad52552b47
alisonamerico <alison.americo@gmail.com>
2017-04-15 22:12:06 -0300

    adicionando middleware para o carrinho de compras

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -58,6 +58,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'checkout.middleware.cart_item_middleware',
+
 ]
 
 ROOT_URLCONF = 'djangoecommerce.urls'
